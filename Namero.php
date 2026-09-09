0b312908bc9700375332247e3deaaca00ae94a22
wissam7716-sketch <wissam7716@gmail.com>
2026-09-10 01:50:12 +0300

    Update Namero.php

--- Namero.php
+++ Namero.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // استقبال طلب تليجرام وفك تشفيره دون إحداث أخطاء
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
